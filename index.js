f0c518eab4424f3f6e8bde4f85201aa3435d832c
bhuvana <bnm108143@gmail.com>
2026-06-18 21:44:52 +0530

    basics of backend

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express')
-import express from "express"
+
 const app = express()
-const port = 3000
+const port = 4000
 
 app.get('/' , (req , res) => {
     res.send("Helloo")
+})
+
+app.get('/twitter' , (req , res) => {res.send('hiteshdotcom')})
+
+app.get('/login' ,(req , res) => {
+    res.send('<h1>Please Login at sharade<h1>')
 })
 
 app.listen(port  , () => {
